28ccf4b3b1acee9e2e5daa0edd2e35455b94e55c
yagorosestolato <yagorosestolato@hotmail.com>
2024-02-01 21:41:29 -0300

    criação da pagina principal, sistema de login e produto controller

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <<div class="card border">
+        <div class="card-body">
+            <h5 class="card-title">Lista de Produtos</h5>
+            <table class="table table-bordered table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Descrição</th>
+                        <th>Preço</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($produto as $produto)
+                        <tr>
+                            <td>{{ $produto->id }}</td>
+                            <td>{{ $produto->name }}</td>
+                            <td>{{ $produto->description }}</td>
+                            <td>R$ {{ number_format($produto->price, 2, ',', '.') }}</td>
+                            <td>
+                                <a href="/produtos/editar/{{ $produto->id }}" class="btn btn-primary">Editar</a>
+                                <a href="/produtos/apagar/{{ $produto->id }}" class="btn btn-danger">Excluir</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="text-center">
+                <a href="/diretor" class="btn btn-primary mb-2">Voltar</a><br>
+                <a href="/produtos/novo" class="btn btn-success mb-2">Cadastrar Novo Produto</a><br>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EscolaSHOP</title>
+    <!-- Add Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+
+<body class="antialiased">
+    <div
+        class="d-flex align-items-center justify-content-center min-vh-100 bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        @if (Route::has('login'))
+            <div class="text-center">
+                @auth
+                    <a href="{{ url('/home') }}" class="btn btn-primary mt-3">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="btn btn-primary mt-3">Log in</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="btn btn-secondary mt-3">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+    <!-- Add Bootstrap JS and Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
