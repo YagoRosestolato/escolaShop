7e6c26944a336b02857c8d365ff3f1a5c557ca88
yagorosestolato <yagorosestolato@hotmail.com>
2024-02-02 23:00:59 -0300

    estilazando a pagina home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <<div class="container mt-4">
+    <div class="container mt-4">
         <div class="row justify-content-center">
 
-            <div class="col-md-4 mb-4 d-flex flex-column">
-                <div class="card flex-fill">
+            <div class="col-md-4 mb-4">
+                <div class="card d-flex flex-column h-100">
                     <div class="card-body">
                         <h5 class="card-title">Área do Diretor</h5>
                         <p class="card-text">Bem-vindo à área do diretor. Gerencie produtos e mais.</p>
                     </div>
-                </div>
-                <div class="mt-3">
-                    <a href="{{ route('app.diretor') }}" class="btn btn-primary btn-block">Acessar</a>
+                    <div class="mt-auto">
+                        <a href="{{ route('app.diretor') }}" class="btn btn-primary btn-block">Acessar</a>
+                    </div>
                 </div>
             </div>
 
-
-            <div class="col-md-4 mb-4 d-flex flex-column">
-                <div class="card flex-fill">
+            <div class="col-md-4 mb-4">
+                <div class="card d-flex flex-column h-100">
                     <div class="card-body">
                         <h5 class="card-title">Área do Fornecedor</h5>
                         <p class="card-text">Gerencie fornecedores e mais nesta área.</p>
                     </div>
-                </div>
-                <div class="mt-3">
-                    <a href="{{ route('app.fornecedor') }}" class="btn btn-primary btn-block">Acessar</a>
+                    <div class="mt-auto">
+                        <a href="{{ route('app.fornecedor') }}" class="btn btn-primary btn-block">Acessar</a>
+                    </div>
                 </div>
             </div>
+
         </div>
     </div>
 @endsection
